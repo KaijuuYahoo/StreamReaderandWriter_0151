1c68dcde1aed3c30a060fe3e364f97e2e94a75b8
KaijuuYahoo <muhammad.rasyid.ft24@mail.umy.ac.id>
2025-06-10 18:34:55 +0700

    Menambahkan logic exception handling pada bagian try

--- expctionStandard.cpp
+++ expctionStandard.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
-#include<string>
 #include<exception>
+#include<array>
 using namespace std;
 
-mai
+int main(int argc, char const *argv[])
+{
+    cout << "Awal Program " << endl; //penanda 1:...
+    try
+    {
+        array <int,3> data={10,20,30};
+        //pesan array integer 3 elemen
+        cout << data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    return 0;
+}
